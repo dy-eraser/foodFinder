e4c0753293d80c272ec3bde6b24c4854f73c5058
Daniel <dy-eraser@users.noreply.github.com>
2025-08-08 11:17:14 +0330

    created controller

---
+++ src/main/java/free/task/foodFinder/configuration/EnvironmentVariables.java
@@ -0,0 +1,22 @@
+package free.task.foodFinder.configuration;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EnvironmentVariables {
+
+	private final Environment env;
+
+	public String getGeoapifyApiKey() {
+		return env.getRequiredProperty("geoapify.api.key");
+	}
+
+	public String getGeoapifyBaseUrl() {
+		return env.getRequiredProperty("geoapify.base.url");
+	}
+
+}
---
+++ src/main/java/free/task/foodFinder/controller/PlaceController.java
@@ -0,0 +1,18 @@
+package free.task.foodFinder.controller;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class PlaceController {
+
+	public List<PlaceDetails> getPlaceDetails() {
+
+	}
+}
